ea2698c7637e94e373f0f3c98593bf6425d1a88b
Siviya Stoyanova <silvia.s.stoianova@gmail.com>
2023-12-01 21:37:32 +0200

    Create the basic configuration and the initial structure.

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ReactDOM, StrictMode } from "react";
+import App from "./App";
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
